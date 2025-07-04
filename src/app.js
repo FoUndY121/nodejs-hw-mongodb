93ba440c2ef04efe76092be917ab072be05eae47
FounDy <vkadgcundgd@gmail.com>
2025-07-04 03:28:30 +0200

    Замена morgan на pino

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const morgan = require('morgan');
 const cors = require('cors');
+const pino = require('pino-http')();
 const contactsRouter = require('./routes/contacts');
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(pino); // Заменяет morgan
 app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
